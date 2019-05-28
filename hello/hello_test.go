4631b55f7e567c1607b6a0cb63fd9e378f8fd7e4
sydernee <thelunatic42@gmail.com>
2019-05-28 14:06:06 +0000

    Refactor Hello example

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Andrea")
-	want := "Hello, Andrea"
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Andrea")
+		want := "Hello, Andrea"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
