8b69c78db9a51a5a43f8df3886279b3f7832c360
sydernee <thelunatic42@gmail.com>
2019-05-29 09:18:42 +0000

    Refactor test repeat example

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,12 +3,19 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-
-	if repeated != expected {
-		t.Errorf("Expected '%s' but got '%s'", expected, repeated)
+	assertCorrectMessage := func(t *testing.T, repeated, expected string) {
+		t.Helper()
+		if repeated != expected {
+			t.Errorf("Expected '%s' but got '%s'", expected, repeated)
+		}
 	}
+
+	t.Run("Repeat 5 times the test", func(t *testing.T) {
+		repeated := Repeat("a")
+		expected := "aaaaa"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
 }
 
 func BenchmarkRepeat(b *testing.B) {
